22a1c6a48843a6e13cf18b4c4209a9421d3edba4
Stewart Pavitt <stewart.pavitt@gmail.com>
2021-04-08 13:16:43 +0100

    #3272 Basic Paylist management added

--- Controllers/PlaylistController.cs
+++ Controllers/PlaylistController.cs
@@ -23,11 +23,7 @@ namespace OpenReferrals.Controllers
         }
 
         [HttpGet]
-<<<<<<< Updated upstream
-        [Route("{userId")]
-=======
         [Route("{userId}")]
->>>>>>> Stashed changes
         public IActionResult Get(string userId)
         {
             var playList = _playListRepo.FindById(userId);
